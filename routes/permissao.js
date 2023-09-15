d3ba9a0ffc0f323ce31be8f08b418c30f4a50185
Carlos <142271796+end223@users.noreply.github.com>
2023-09-15 10:40:21 -0300

    CRUD de Permissoes implementado

--- routes/permissao.js
+++ routes/permissao.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const PermissaoController = require('../controllers/PermissaoController');
 
 const router = Router();
 
 router
-    .post('/permissao')
-    .get('/permissao')
-    .get('/permissao/:id')
-    .put('/permissao/:id')
-    .delete('/permissao/:id')
+    .post('/permissao', PermissaoController.cadastrar)
+    .get('/permissao', PermissaoController.buscarTodasPermissoes)
+    .get('/permissao/id/:id', PermissaoController.buscarPermissaoPorId)
+    .delete('/permissao/id/:id', PermissaoController.deletarPermissaoPorId)
+    .put('/permissao/id/:id', PermissaoController.editarPermissao)
 
 module.exports = router    
