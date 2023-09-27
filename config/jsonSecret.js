0c9b650932103e3fde86fbedff745788af913d2a
Carlos <142271796+end223@users.noreply.github.com>
2023-09-27 13:44:56 -0300

    Autenticação Tutores e Abrigos

--- config/jsonSecret.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
-  refreshSecret: '38816a4bbe78aa3dbff468dc0d08c217df8cc3b4f3c3fd9f139bab84d69dff5c',
-};
