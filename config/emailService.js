add379e40c0427e66c023813825739e4651973e8
Carlos <142271796+end223@users.noreply.github.com>
2023-10-12 11:47:29 -0300

    Salvando minhas alterações atuais

--- config/emailService.js
+++ config/emailService.js
@@ -4,17 +4,27 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: 'smtp-mail.outlook.com',
   port: 587,
+  secure: false,
   auth: {
     user: process.env.USER,
-    pass: process.env.PASS,
+    pass: process.env.PASS, 
   },
 });
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log('Erro ao verificar a configuração do transporte:', error);
+  } else {
+    console.log('Configuração do transporte verificada com sucesso:', success);
+  }
+});
+
 
 async function sendConfirmationEmail(userId, userEmail) {
   try {
     const confirmationLink = `${process.env.BASE_URL}/confirm?userId=${userId}`;
     const mailOptions = {
-      from: '<adopet@adopet.com.br>',
+      from: 'ivanilde.pe.sousa2@hotmail.com',
       to: userEmail,
       subject: 'Confirmação de E-mail',
       text: `Clique no link a seguir para confirmar seu e-mail: ${confirmationLink}`,
@@ -30,7 +40,7 @@ async function sendConfirmationEmail(userId, userEmail) {
 async function enviarEmailRedefinicaoSenha(userEmail, resetLink) {
   try {
     const mailOptions = {
-      from: '<adopet@adopet.com.br>',
+      from: 'ivanilde.pe.sousa2@hotmail.com',
       to: userEmail,
       subject: 'Redefinição de Senha',
       text: `Clique no link a seguir para redefinir sua senha: ${resetLink}`,
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
 const passport = require('passport');
-const cors = require('cors'); // Importe o pacote cors
-
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 3001;
