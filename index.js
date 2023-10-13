e0f23d2bce7f3127cc56f4c03846ca448b24b70d
Carlos <142271796+end223@users.noreply.github.com>
2023-10-13 16:21:18 -0300

    Build

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({ origin: 'https://adopet-spwr.onrender.com' }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://adopet-spwr.onrender.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 app.use(passport.initialize());
 
