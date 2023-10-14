d7b2efd1cc692db77b2f4c7ca87b7519c4fb909d
Carlos <142271796+end223@users.noreply.github.com>
2023-10-13 22:18:53 -0300

    building

--- config/redis.js
+++ config/redis.js
@@ -1,17 +1,6 @@
 const Redis = require('ioredis');
 require('dotenv').config();
 
-const redisClient = new Redis({
-  sentinels: [
-    {
-      host: 'oregon-redis.render.com',
-      port: 6379,
-    },
-  ],
-  name: 'red-ckkugtav7m0s73cruim0',
-  password: 'QZj2QpxfG6edSSsI9iE9KeY5bmtRmk3R',
-  tls: {},
-});
-
+const redisClient = new Redis('process.env.REDIS_HOST');
 
 module.exports = redisClient;
